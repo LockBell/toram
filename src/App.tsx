1917a34fc66cd79acdb1fbfe8c876c38501b0c0c
PersesTitan <torampersesTitan@gmail.com>
2024-10-19 14:09:27 +0900

    add basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Outlet, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 import {Statting} from "./components/Statting.tsx";
 import {createTheme, ThemeProvider, useColorScheme} from "@mui/material";
 import {Admin} from "./components/Admin.tsx";
@@ -19,19 +19,21 @@ function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Routes>
-                <Route path='statting' element={<Statting/>}/>
-                <Route path='admin' element={<Outlet/>}>
-                    <Route path='' element={<Admin/>}/>
-                    <Route path='users' element={<Outlet/>}>
-                        <Route path='' element={<AdminUsers/>}/>
-                        <Route path='create' element={<AdminUser/>} id='create'/>
-                        <Route path=':id' element={<AdminUser/>}/>
-                        <Route path=':id/edit' element={<AdminUser/>}/>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Routes>
+                    <Route path='statting' element={<Statting/>}/>
+                    <Route path='admin' element={<Outlet/>}>
+                        <Route path='' element={<Admin/>}/>
+                        <Route path='users' element={<Outlet/>}>
+                            <Route path='' element={<AdminUsers/>}/>
+                            <Route path='create' element={<AdminUser/>} id='create'/>
+                            <Route path=':id' element={<AdminUser/>}/>
+                            <Route path=':id/edit' element={<AdminUser/>}/>
+                        </Route>
                     </Route>
-                </Route>
-                <Route path='/login' element={<Login/>}/>
-            </Routes>
+                    <Route path='/login' element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
             <footer>
                 <p>
                     <a>개인정보 처리 방침</a>
